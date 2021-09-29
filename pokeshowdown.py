4e135a652f809d3e09414e3a0ec7dca8f0f10020
james051898 <74512319+james051898@users.noreply.github.com>
2021-09-29 08:34:54 -0400

    Main driver of pokeshowdown

--- pokeshowdown.py
+++ pokeshowdown.py
@@ -4,6 +4,12 @@ import supportive as s
 import pandas as pd
 from openpyxl.utils.dataframe import dataframe_to_rows
 from pandas import ExcelWriter as w
+
+# This program is used to get the opponents team from my chat log, which is stored locally and updated live.
+# This program is a much worse version of what I should have from selenium. 
+
+# ** For now, it also serves as the main driver of the project **
+
 file = "C:/Users/kingj\Documents/My Games/Pokemon Showdown/Logs/2021-09/pm-csc2405jbm.txt"
 
 infile = open(file, "r+")
@@ -14,7 +20,7 @@ wordlist = (infile.read()).split('\n')
 for line in wordlist:
     if substring in line:
         team = line
-# infile.truncate(0)
+# infile.truncate(0)   # Deletes contents in file
 infile.close()
 team = team.split(':')
 team = team[2]
